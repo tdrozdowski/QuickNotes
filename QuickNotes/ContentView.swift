262874181225d16a72cf71882cf87643b1e80694
Terry Drozdowski <terry@xymox.dev>
2022-09-02 23:07:43 -0700

    Initial Commit

---
+++ QuickNotes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  QuickNotes
+//
+//  Created by Terry Drozdowski on 9/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickNotes/QuickNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickNotesApp.swift
+//  QuickNotes
+//
+//  Created by Terry Drozdowski on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct QuickNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
